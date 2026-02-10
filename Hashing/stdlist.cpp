58c4370702ab224ac81da237b31a8ddfb562ddee
MTaqiPirani <taqi2110e@aptecgdn.net>
2026-02-10 12:08:16 +0500

    codes

--- Hashing/stdlist.cpp
+++ Hashing/stdlist.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<list>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
 
 using namespace std;
 
@@ -10,7 +10,11 @@ struct Student
     int id;
     string name;
 
-    Student(int id, string name) : id(id), name(name) {}
+    Student(int id, string name)
+    {
+        this->id = id;
+        this->name = name;
+    }
 };
 
 class studentHashtable
@@ -18,7 +22,7 @@ class studentHashtable
     vector<list<Student>> hashtable;
     int buckets;
 
-    public:
+public:
     studentHashtable(int size)
     {
         buckets = size;
@@ -27,36 +31,76 @@ class studentHashtable
 
     int hashvalue(int id)
     {
-        return id%buckets;
+        return id % buckets;
     }
 
     void add(int id, string name)
     {
         int idx = hashvalue(id);
-        hashtable[idx].push_back(Student(id,name));
+        hashtable[idx].push_back(Student(id, name));
     }
 
     Student* search(int id)
     {
         int idx = hashvalue(id);
-        for(Student& s : hashtable[idx])
+
+        for (Student& s : hashtable[idx])
         {
-            if(s.id == id)
-            {
+            if (s.id == id)
                 return &s;
-            }
         }
+        return nullptr;
     }
 
     void deleteStudent(int id)
     {
         int idx = hashvalue(id);
-        
+
+        for (auto it = hashtable[idx].begin(); it != hashtable[idx].end(); ++it)
+        {
+            if (it->id == id)
+            {
+                hashtable[idx].erase(it);
+                return;
+            }
+        }
+    }
+
+    void display()
+    {
+        for (int i = 0; i < buckets; i++)
+        {
+            cout << "Bucket " << i << ": ";
+            for (Student s : hashtable[i])
+            {
+                cout << "[" << s.id << ", " << s.name << "] ";
+            }
+            cout << endl;
+        }
     }
 };
 
-
 int main()
 {
+    studentHashtable ht(10);
 
+    ht.add(1, "Ali");
+    ht.add(11, "Ahmed");   // collision
+    ht.add(21, "Usman");   // collision
+    ht.add(5, "Sara");
+
+    cout << "Hash Table:\n";
+    ht.display();
+
+    Student* s = ht.search(11);
+    if (s != nullptr)
+        cout << "\nFound: " << s->id << " " << s->name << endl;
+    else
+        cout << "\nStudent not found\n";
+
+    ht.deleteStudent(11);
+    cout << "\nAfter Deleting ID 11:\n";
+    ht.display();
+
+    return 0;
 }
